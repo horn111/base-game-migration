5474113bd418db492ae34d51594c5f51f8a94900
horn111 <horn111@users.noreply.github.com>
2026-07-09 20:48:47 +0300

    Add Talent App project verification meta

--- apps/demo/src/app/layout.tsx
+++ apps/demo/src/app/layout.tsx
@@ -9,6 +9,10 @@ export const metadata: Metadata = {
   icons: {
     icon: "/bgm-gamepad-logo.svg",
   },
+  other: {
+    "talentapp:project_verification":
+      "8f6c4adcd47c3162c89d0df41b93a8c03cca6027df58aa67582cf5b792f8a2e38e68dca0fb8e8186bb64152d836f8277e549ac0586e34fc21005a1151529444f",
+  },
 };
 
 export default function RootLayout({
